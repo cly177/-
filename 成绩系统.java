8ec5c689e5c196c2f09d339dfbae354a339d6386
cly177 <80100814+cly177@users.noreply.github.com>
2021-03-19 18:54:59 +0800

    Add files via upload

--- 成绩系统.java
+++ 成绩系统.java
@@ -7,7 +7,7 @@ public class 成绩系统 {
 	public static void main(String[] args) {
 		
 		int choose=menu();
-		while(choose!=5) {
+		while(choose!=6) {
 			switch(choose) {
 			case 1:  
 				enterResults()  ;break;
@@ -26,12 +26,15 @@ public class 成绩系统 {
 				else
 				highestGrade(score)  ;break;
 			case 4:
+				
+				lowestGrade (score)  ;break;
+			case 5:
 				if(score==null) {
 					System.out.println("没有输入成绩，请选择1");
 					break;
 				}
 				else
-				lowestGrade (score)  ;break;
+				paixu(score);break;
 			default:   System.out.println("输入错误，重新输入");		
 			}
 			choose=menu();
@@ -46,8 +49,9 @@ public class 成绩系统 {
 		System.out.println("2.class average");
 		System.out.println("3.highest grade");
 		System.out.println("4.lowest grade");
-		System.out.println("5.exit");
-		System.out.println("请输入（1~5）");		
+		System.out.println("5.排序");
+		System.out.println("6.exit");
+		System.out.println("请输入（1~6）");		
 		Scanner scan=new Scanner(System.in);		
 		choice=scan.nextInt();
 		return choice;
@@ -65,7 +69,6 @@ public class 成绩系统 {
 				if(score[i]<=0||score[i]>100)  System.out.println("输入错误");
 				else flag=0;
 			}
-			
 		}
 		
 	}
@@ -97,6 +100,23 @@ public class 成绩系统 {
 			if(score[i]<num) num=score[i];
 		}
 		System.out.println("最小值为"+num);
+		
+	}
+	public static void paixu(int score[]) {
+		int t;
+		for (int i = 0; i < score.length-1; i++) {
+			for (int j = 0; j < score.length-1-i; j++) {
+				if(score[j]>score[j+1]) {
+					t=score[j];
+					score[j]=score[j+1];
+					score[j+1]=t;
+				}
+			}
+		}
+		for (int i = 0; i < score.length; i++) {
+			System.out.println(score[i]);
+			
+		}
 		
 	}
 
